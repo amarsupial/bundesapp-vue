65ab312bd55c6ff4a538749d829185c50dfb241c
Tobias Michaely <t_michaely@web.de>
2021-11-17 22:07:52 +0100

    added features section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <HeroSection />
     <Incentives />
+    <Features />
   <!-- </div> -->
 </template>
 
@@ -12,12 +13,14 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeroSection from '../components/HeroSection.vue'
 import Incentives from '../components/Incentives.vue'
+import Features from '../components/Features.vue'
 
 export default {
   name: 'Home',
   components: {
     HeroSection,
-    Incentives
+    Incentives,
+    Features
   }
 }
 </script>
