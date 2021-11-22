3301e4be772b1233fe701ac3bb9d673e7ff8a726
Tobias Michaely <t_michaely@web.de>
2021-11-22 22:16:42 +0100

    changed font color to white

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -138,7 +138,7 @@
     <!-- Logo Section -->
     <div class="bg-dpbm-blue">
       <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <h2 class="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">Trusted by over 26,000 forward-thinking companies</h2>
+        <h2 class="text-center text-white text-sm font-semibold uppercase tracking-wide">Trusted by over 26,000 forward-thinking companies</h2>
         <div class="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
